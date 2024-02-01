cb57c885f3e8d0b4e1519ce5d689875ab1357d84
Geoffrey Samper <geoffrey.samper@mediahuis.be>
2024-02-01 09:31:25 +0100

    added readme

--- datadog.cjs
+++ datadog.cjs
@@ -1,21 +1,17 @@
 
 //this must be one of the first module to be loaded
 const tracer = require('dd-trace').init()
-const httpPlugins = ["http", "https", "http2", "fetch"];
+
 
 //enable allow datadog to intercept
-// require("node:http")
-// require("node:https")
+ require("node:http")
+ require("node:https")
 
+const httpPlugins = ["http", "https", "http2", "fetch"];
 
-hooks = { request: intercept }
 for (plugin of httpPlugins) {
     tracer.use(plugin, {
-        hooks: {request : intercept}
+        hooks: {request : (span, req,res) =>   console.log("INTERCEPTED")}
     })
 
-}
-
-function intercept(span, req,res){
-    console.log("INTERCEPTED")
 }
